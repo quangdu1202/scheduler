ae98ef43a43a478893fd3d1db946ae7d28dba420
quangdd <quangdd@avadagroup.com>
2024-04-20 14:13:15 +0000

    update

--- resources/views/practice_class/index.blade.php
+++ resources/views/practice_class/index.blade.php
@@ -4,21 +4,21 @@
     <div class="container h-100 right-content">
 
         <!-- Page Header -->
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Practice Class Management</h1>
+        <div class="py-3 mb-3 border-bottom">
+            <h1 class="h2">Practice Classes Management</h1>
         </div>
 
         <div class="top-nav nav mb-3 d-flex align-items-center">
             <!-- Action Buttons (Add new, etc.) -->
             <div class="action-buttons">
-                <a href="{{route('practice-classes.create')}}" id="add-class-new" class="btn btn-primary btn-sm" type="button">
+                <button id="add-room-form-toggle" class="btn btn-primary btn-sm" type="button">
                     <i class="lni lni-circle-plus align-middle"></i> Add new
-                </a>
+                </button>
             </div>
             <div class="vr mx-5"></div>
             <form id="practice-class-filter" action="#" class="d-flex align-items-center">
-                <label for="module-select" class="me-2 text-nowrap fw-bold">Module:</label>
-                <select name="module" id="module-select" class="form-select">
+                <label for="practice-room-select" class="me-2 text-nowrap fw-bold">Module:</label>
+                <select name="practice-room" id="practice-room-select" class="form-select">
                     <option value="-1" selected>--- Select Module ---</option>
                     <option value="1">Nhập môn lập trình máy tính</option>
                     <option value="2">Kỹ thuật lập trình</option>
@@ -30,15 +30,14 @@
 
         <!-- Practice Classes Table -->
         <div class="table-responsive">
-            <table id="module-management-table" class="table table-bordered table-hover w-100 text-center">
+            <table id="pclass-management-table" class="table table-bordered table-hover w-100 text-center">
                 <thead class="thead-light">
                 <tr>
                     <th>#</th>
-                    <th>Module ID</th>
-                    <th>Practice Class Name</th>
-                    <th>Schedule Date</th>
+                    <th>Class Name</th>
+                    <th>Start Date</th>
                     <th>Session</th>
-                    <th>Practice Room</th>
+                    <th>Room</th>
                     <th>Teacher</th>
                     <th>Recurring</th>
                     <th>QTY</th>
@@ -46,87 +45,78 @@
                 </tr>
                 </thead>
                 <tbody>
-                @foreach($practiceClasses as $key => $practiceClass)
-                    <tr data-pclass-id="{{ $practiceClass->id }}">
-                        <td>{{ $key+1 }}</td>
-                        <td>{{ $practiceClass->module_id }}</td>
-                        <td>{{ $practiceClass->practice_class_name }}</td>
-                        <td>{{ $practiceClass->schedule_date }}</td>
-                        <td>{{ $practiceClass->session }}</td>
-                        <td>{{ $practiceClass->practice_room_id }}</td>
-                        <td>{{ $practiceClass->teacher_id }}</td>
-                        <td>{{ $practiceClass->recurring_id }}</td>
-                        <td>{{ $practiceClass->registered_qty }}</td>
-                        <td>
-                            <button type="button" class="table-row-btn module-btn-info btn btn-success btn-sm" data-pclass-id="{{ $practiceClass->id }}" title="Module Info">
-                                <i class="fa-solid fa-building-circle-exclamation"></i>
-                            </button>
-                            <button type="button" class="table-row-btn module-btn-edit btn btn-primary btn-sm" data-pclass-id="{{ $practiceClass->id }}" title="Edit Module Info" style="padding-right: 0.45rem">
-                                <i style="padding-left: 0.05rem" class="lni lni-pencil-alt"></i>
-                            </button>
-                            <button type="button" class="table-row-btn module-btn-delete btn btn-danger btn-sm" data-pclass-id="{{ $practiceClass->id }}" title="Delete Module">
-                                <i class="lni lni-trash-can"></i>
-                            </button>
-                        </td>
-                    </tr>
-                @endforeach
                 </tbody>
             </table>
         </div>
     </div>
 
     <script>
-        $(document).ready(function () {
-            $('#module-management-table').DataTable({
-                layout: {
-                    topEnd: {
-                        search: {placeholder: 'Search'},
-                        buttons: [
-                            'length',
-                            {
-                                extend: 'csv',
-                                exportOptions: {
-                                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8]
-                                }
-                            },
-                            {
-                                extend: 'excel',
-                                exportOptions: {
-                                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8]
-                                }
-                            },
-                            {
-                                extend: 'print',
-                                exportOptions: {
-                                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8]
-                                }
+        //Data table initiate
+        const roomTable = $('#room-management-table').DataTable({
+            ajax: {
+                url: '{{route('practice-rooms.get-json-data')}}',
+                dataSrc: ''
+            },
+            columns: [
+                { data: 'index',  width: '5%'},
+                { data: 'name', type: 'string', width: '30%' },
+                { data: 'location', type: 'string', width: '20%' },
+                { data: 'pc_qty', width: '15%' },
+                { data: 'status', type: 'html', width: '15%' },
+                { data: 'actions', type: 'html', width: '15%' },
+            ],
+            columnDefs: [{
+                "className": "dt-center",
+                "targets": [0, 2, 3, 4, 5]
+            }],
+            layout: {
+                topEnd: {
+                    search: {
+                        placeholder: 'Search anything'
+                    },
+                    buttons: [
+                        'length',
+                        {
+                            extend: 'csv',
+                            exportOptions: {
+                                columns: [0, 1, 2, 3, 4]
                             }
-                        ]
-                    },
+                        },
+                        {
+                            extend: 'excel',
+                            exportOptions: {
+                                columns: [0, 1, 2, 3, 4]
+                            }
+                        },
+                        {
+                            extend: 'print',
+                            exportOptions: {
+                                columns: [0, 1, 2, 3, 4]
+                            }
+                        }
+                    ]
                 },
-                pageLength: -1,
-                columnDefs: [
-                    {"className": "dt-center", "targets": "_all"}
-                ],
-                // columns: [{ width: '35px' }, { width: '130px' }, null, { width: '120px' }, { width: '150px' }, { width: '120px' }],
-                language: {
-                    "info": "Showing _START_ to _END_ of _TOTAL_ modules",
-                    //customize pagination prev and next buttons: use arrows instead of words
-                    'paginate': {
-                        'previous': '<span class="fa fa-chevron-left"></span>',
-                        'next': '<span class="fa fa-chevron-right"></span>'
-                    },
-                    //customize number of elements to be displayed
-                    "lengthMenu": '<select class="form-control input-sm">'+
-                        '<option value="-1">All</option>'+
-                        '<option value="10">10</option>'+
-                        '<option value="20">20</option>'+
-                        '<option value="30">30</option>'+
-                        '<option value="40">40</option>'+
-                        '<option value="50">50</option>'+
-                        '</select> modules per page'
-                }
-            });
+            },
+            pageLength: -1,
+            language: {
+                "info": "Showing _START_ to _END_ of _TOTAL_ rooms",
+                //customize pagination prev and next buttons: use arrows instead of words
+                'paginate': {
+                    'first': '<span class="fa-solid fa-backward-step"></span>',
+                    'previous': '<span class="fa fa-chevron-left"></span>',
+                    'next': '<span class="fa fa-chevron-right"></span>',
+                    'last': '<span class="fa-solid fa-forward-step"></span>'
+                },
+                //customize number of elements to be displayed
+                "lengthMenu": '<select class="form-control input-sm">' +
+                    '<option value="-1">All</option>' +
+                    '<option value="10">10</option>' +
+                    '<option value="20">20</option>' +
+                    '<option value="30">30</option>' +
+                    '<option value="40">40</option>' +
+                    '<option value="50">50</option>' +
+                    '</select> rooms per page'
+            }
         });
     </script>
 @endsection
